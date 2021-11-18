7a1584b6d919e679f06a75ad03fd429758d217cb
pashwiniis <pashwini684@gmail.com>
2021-11-18 21:47:10 +0530

    conditions check using Switch case Statement

--- Program.cs
+++ Program.cs
@@ -9,36 +9,39 @@ namespace EmployeWageComputationProblem
 {
     class EmployeeCheck
     {
+       public const int IS_PART_TIME = 1;
+       public const int IS_FULL_TIME = 2;
+       public const int EMP_RATE_PER_HOUR = 20;
         static void Main(string[] args)
         {
-            int IS_PART_TIME = 1;
-            int IS_FULL_TIME = 2;
-            int EMP_RATE_PER_HOUR = 20;
-
+            
             int empHrs = 0;
             int empWage = 0;
-            Random random = new Random();
+            string typeOfEmployee = "";
 
-            int empCheck = random.Next(0, 3);
-            if (empCheck == IS_PART_TIME)
+
+            Random random = new Random();
+            int randomInput = random.Next(0, 3);
+            switch (randomInput)
             {
                 ///Console.WriteLine("Employee is present");
-                empHrs = 4;
-                
-            }
-            else if (empCheck == IS_FULL_TIME)
-            {
-                empHrs = 8;
-               
-            }
-            else
-            {
-                ///Console.WriteLine("Employee is absent");
-                empHrs = 0;
-                
-            }
+                case IS_PART_TIME:
+                     empHrs = 4;
+                    typeOfEmployee = "Parttime";
+                     break;
+            
+                case IS_FULL_TIME:
+                     empHrs = 8;
+                    typeOfEmployee = "Fulltime";
+                     break;
+            
+                default:
+                     empHrs = 0;
+                    typeOfEmployee = "Absent";
+                     break;
+             }
             empWage = empHrs *  EMP_RATE_PER_HOUR;
-            Console.WriteLine("Employee Wage Per day is :" + empWage);
+            Console.WriteLine(typeOfEmployee + "Employee Wage Per day is :" + empWage);
             Console.ReadLine();
         }
     }
