f9e61c065553035a36e1f17eb5c791643e66fea7
pashwiniis <pashwini684@gmail.com>
2021-11-18 22:11:10 +0530

    Calculating wages for a 20 working days per month

--- Program.cs
+++ Program.cs
@@ -12,36 +12,43 @@ namespace EmployeWageComputationProblem
        public const int IS_PART_TIME = 1;
        public const int IS_FULL_TIME = 2;
        public const int EMP_RATE_PER_HOUR = 20;
+        public const int NO_OF_WORKING_DAYS = 20;
         static void Main(string[] args)
         {
             
             int empHrs = 0;
             int empWage = 0;
+            int totalEmpWage = 0;
             string typeOfEmployee = "";
 
-
-            Random random = new Random();
-            int randomInput = random.Next(0, 3);
-            switch (randomInput)
+            for (int day = 0; day <= NO_OF_WORKING_DAYS; day++)
             {
-                ///Console.WriteLine("Employee is present");
-                case IS_PART_TIME:
-                     empHrs = 4;
-                    typeOfEmployee = "Parttime";
-                     break;
-            
-                case IS_FULL_TIME:
-                     empHrs = 8;
-                    typeOfEmployee = "Fulltime";
-                     break;
-            
-                default:
-                     empHrs = 0;
-                    typeOfEmployee = "Absent";
-                     break;
-             }
-            empWage = empHrs *  EMP_RATE_PER_HOUR;
-            Console.WriteLine(typeOfEmployee + "Employee Wage Per day is :" + empWage);
+
+                Random random = new Random();
+                int randomInput = random.Next(0, 3);
+                switch (randomInput)
+                {
+                    ///Console.WriteLine("Employee is present");
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        typeOfEmployee = "Parttime";
+                        break;
+
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        typeOfEmployee = "Fulltime";
+                        break;
+
+                    default:
+                        empHrs = 0;
+                        typeOfEmployee = "Absent";
+                        break;
+                }
+                empWage = empHrs *  EMP_RATE_PER_HOUR;
+                Console.WriteLine("{0} Employee of day {1} Wage is {2} " ,typeOfEmployee,day,empWage);
+                totalEmpWage = totalEmpWage+empWage;
+            }
+            Console.WriteLine("Employee Wage for {0} day is :{1}", NO_OF_WORKING_DAYS, totalEmpWage);
             Console.ReadLine();
         }
     }
